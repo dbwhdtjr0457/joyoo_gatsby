c0816f97501a71515d6100331bc32ecec8a06437
dbwhdtjr0457 <dbwhdtjr0457@gmail.com>
2023-12-27 14:51:59 +0900

    Gatsby Starter Blog 적용

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,32 +1,36 @@
-import * as React from "react";
-import { Link } from "gatsby";
+import * as React from "react"
+import { Link } from "gatsby"
 
-const Layout = ({ pageTitle, children }) => {
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  const isRootPath = location.pathname === rootPath
+  let header
+
+  if (isRootPath) {
+    header = (
+      <h1 className="main-heading">
+        <Link to="/">{title}</Link>
+      </h1>
+    )
+  } else {
+    header = (
+      <Link className="header-link-home" to="/">
+        {title}
+      </Link>
+    )
+  }
+
   return (
-    <div>
-      <title>{pageTitle}</title>
-      <nav>
-        <ul className="flex">
-          <li className="mr-6">
-            <Link to="/" className="text-blue-500 hover:text-blue-800">
-              Home
-            </Link>
-          </li>
-          <li className="mr-6">
-            <Link to="/about" className="text-blue-500 hover:text-blue-800">
-              About
-            </Link>
-          </li>
-          <li className="mr-6">
-            <Link to="/contact" className="text-blue-500 hover:text-blue-800">
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main className="mt-4">{children}</main>
+    <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <header className="global-header">{header}</header>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </footer>
     </div>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
