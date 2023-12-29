c46398a6592eb5a4a9e1621a106d9f7d74b81a1e
Jongseok Yoo <joyoo@c5r5s7.42seoul.kr>
2023-12-29 23:26:41 +0900

    temp

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,13 +16,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/contents/blog`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/contents/blog`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-emotion`,
