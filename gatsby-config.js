77d8b6ef1cd3fe23fed8e4fc989b73b7ad6e6642
Jongseok Yoo <joyoo@c5r5s7.42seoul.kr>
2023-12-29 23:24:18 +0900

    temp

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/contents`,
+        path: `${__dirname}/contents/blog`,
       },
     },
     `gatsby-transformer-sharp`,
